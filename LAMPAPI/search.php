c9f68454a9979447170833b5f4884134ad752046
Ryan Pattillo <ryanpattillo@Ryans-MacBook-Pro-2.local>
2021-01-24 21:36:13 -0500

    Update

--- LAMPAPI/search.php
+++ LAMPAPI/search.php
@@ -16,7 +16,7 @@
     
     else
     {
-        $sql = "SELECT contactFirstName,contactLastName,CID FROM Contacts where contactFirstName LIKE '%" . $inData["search"] . "%' OR contactLastName like '%" . $inData["search"] . "%' AND UserID=" .$inData["userId"];
+        $sql = "SELECT contactFirstName,contactLastName,CID FROM Contacts where contactFirstName LIKE '%" .$inData["search"] . "%' OR contactLastName like '%" . $inData["search"] . "%' AND ID=" .$inData["userId"]."'";
         
         $result = $conn->query($sql); #Will return an array
         
