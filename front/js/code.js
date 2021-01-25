13fde09e275af0795a7b35eac138d2ed9d40c14d
Ryan Pattillo <ryanpattillo@Ryans-MacBook-Pro-2.local>
2021-01-25 17:50:44 -0500

    Update

--- front/js/code.js
+++ front/js/code.js
@@ -130,12 +130,12 @@ function fillSearchBox(JSONObject) {
 	var contactFirstName = JSONObject.results[i].contactFirstName
 	var contactLastName = JSONObject.results[i].contactLastName
 	contacts.push(JSONObject.results[i].contactID)	
-	var button = "<div class = 'row w-100 h-auto p-0 g-0' onClick = showContact('" + contactFirstName + "','" + contactLastName + "','" + i +"')>" +
+	var button = "<div class = 'row w-100 border border-2 border-primary rounded-3 h-auto p-1 g-0' bonClick = showContact('" + contactFirstName + "','" + contactLastName + "','" + i +"')>" +
     "<div class = 'col-4 p-0 g-0'>" +
 	"<div class = 'circle'><h3>"+ contactFirstName[0] + contactLastName[0] +"</h3></div>" + "</div>" + 
 	"<div class = 'col-7 d-flex flex-column p-0 g-0'>" +
-	"<h2 style = 'padding-left:5%'>" + contactFirstName + "</h2>" +
-	"<h3 style = 'padding-left:20%'>" + contactLastName + "</h3> </div>" +
+	"<h4 style = 'padding-left:5%'>" + contactFirstName + "</h4>" +
+	"<h4 style = 'padding-left:20%'>" + contactLastName + "</h4> </div>" +
 	"<div class = 'col-1'>" + "</div>"
 	$("#contacts").append(button)
 	}
