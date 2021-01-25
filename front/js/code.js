06ae8fd11a4b45e0a664af278da373aced383708
Ryan Pattillo <ryanpattillo@Ryans-MacBook-Pro-2.local>
2021-01-24 20:55:10 -0500

    update

--- front/js/code.js
+++ front/js/code.js
@@ -344,8 +344,5 @@ function readCookie()
 	{
 		window.location.href = "index.html";
 	}
-	else
-	{
-		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
-	}
+	
 }
