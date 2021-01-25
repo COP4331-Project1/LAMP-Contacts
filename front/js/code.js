8f0577c1a3bb685669972ad74623adefed598868
Ryan Pattillo <ryanpattillo@Ryans-MacBook-Pro-2.local>
2021-01-24 23:11:30 -0500

    Update

--- front/js/code.js
+++ front/js/code.js
@@ -161,7 +161,7 @@ function createInfoBoxes() {
 	"<div class = 'informationBox'>" +
 	  "<span class = 'titleBox'>" +
 		"<h3 id = 'contactAttribute'>First</h3>" + 
-		"<i class='bi-pencil' onclick = 'modify(" + "'contactFirstName'" + ")'></i>" +
+		"<i class='bi-pencil' onclick = 'modify(" + '"contactFirstName"' + ")'></i>" +
 	"</span> " +
 	  "<div class = 'contactFirstName'>" +
 	  "<p>" + contactFirstName + "</p>" + "</div>" + "</div>"  +
