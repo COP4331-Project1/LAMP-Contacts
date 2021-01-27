b1a073b887b567f54dd73bfa48bfc95f2854d306
Gregory Freitas <gregfreitas1997@gmail.com>
2021-01-27 12:02:13 -0500

    sql Syntax

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -33,7 +33,7 @@ else
 	{
 
 		// Generates the mySql command to insert the request info.
-        $sql = "insert into Contacts (ID,contactFirstName,contactLastName,phoneNumber,address,email) VALUES (". $inData["ID"] . ",'" . $conFirstName . "','" . $conLastName . "','" . $phoneNumber . "','" . $address . "','" . $email ."')";
+        $sql = "insert into Contacts (ID,contactFirstName,contactLastName,phoneNumber,address,email) VALUES (". $inData["ID"] . ",'" . $conFirstName . "','" . $conLastName . "'," . $phoneNumber . ",'" . $address . "','" . $email ."')";
 		
 		if($result = $conn->query($sql) != TRUE)
 		{
