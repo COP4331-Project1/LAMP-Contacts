c0a8cfa0f3f7dfa884d5601cbca3df804c6c3101
Ryan Pattillo <ryanpattillo@Ryans-MacBook-Pro-2.local>
2021-01-30 18:22:37 -0500

    Update

--- front/js/code.js
+++ front/js/code.js
@@ -4,6 +4,7 @@ email = ""
 userName = ""
 ID  = 0
 
+
 var contacts = []
 
 function openHTTP(url,action){
@@ -26,7 +27,7 @@ function login() {
 		return;
 	}
 	url = "http://159.203.70.233/LAMPAPI/Login.php"
-
+	
   	try {
 		var xhr = openHTTP(url,"POST")
 	    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
@@ -34,13 +35,13 @@ function login() {
 		xhr.onreadystatechange = function() {
 
 		if(this.readyState == 4 && this.status == 200) {
-
+		
 		var jsonObject = JSON.parse(xhr.responseText); //Parses the response text, converts to javascript object
-
+		
 		ID = jsonObject.ID; //Gets the user ID form the databsae
-
-		if( ID < 1 ) //Represents an error
-		{
+		
+		if( ID < 1 ) //Represents an error 
+		{		
 			$(".errorBar").empty();
 			$(".errorBar").append("<p id = 'errorText'> Incorrect Username/Password </p>");
 			return;
@@ -54,9 +55,9 @@ function login() {
 		}
 		}
 		}
-		xhr.send(jsonData); //Will send the data and when the state changes will recieve a response
+		xhr.send(jsonData); //Will send the data and when the state changes will recieve a response	
 	}
-    catch(err){
+    catch(err){	
 
 		$(".errorBar").append("<p> Internal Server Error </p>" );
 		}
@@ -79,8 +80,8 @@ function register() {
 	 return;
 
 	}
-	var jsonData = JSON.stringify({"firstName":firstName,"lastName":lastName,"userName":userName , "password":  password , "email": email}) //Json
-
+	var jsonData = JSON.stringify({"firstName":firstName,"lastName":lastName,"userName":userName , "password":  password , "email": email}) //Json 
+	
 	try {
 
 		var xhr = openHTTP(url,"POST")
@@ -88,10 +89,10 @@ function register() {
 		xhr.onreadystatechange = function() {
 		if(this.readyState == 4 && this.status == 200) {
 
-		var jsonObject = JSON.parse(xhr.responseText);
+		var jsonObject = JSON.parse(xhr.responseText); 
 
 		if(jsonObject.error == "This username already exists, try another one."){
-		$(".errorBar").empty();
+		$(".errorBar").empty();	
 		$(".errorBar").append("<p id = 'errorText'>" + jsonObject.error + "</p>")
 		return;
 		}
@@ -116,27 +117,27 @@ function search() {
 	var jsonData = JSON.stringify({"ID":ID, "search":search})
 	var xhr = openHTTP(url,"POST");
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-
-	try {
-
+	
+	try {	
+	
 		xhr.onreadystatechange = function() {
-
+			
 		if(this.readyState == 4 && this.status == 200) {
-
+			
 		var JSONObject = JSON.parse(xhr.responseText); //Parses the response text, converts to javascript object
-
+		
 		 //Will send the data and when the state changes will recieve a response
 		fillSearchBox(JSONObject);
 		}
 		};
-		xhr.send(jsonData);
-	}
+		xhr.send(jsonData);	
+	}	
 	catch(err) {
 
 		var errorMessage = "<h3>" + err.message + "</h3>"
-		console.log(errorMessage)
+		console.log(errorMessage)		
 		$("#contacts").append(errorMessage)
-
+		
 	}
 
 }
@@ -145,22 +146,22 @@ function fillSearchBox(JSONObject) {
 
 	contacts = []
 	$("#contacts").empty()
-
+	
 	var entries = JSONObject.entries
 
 	if(entries == 0){
 		$("#contacts").append("<p> 0 results </p>")
 		return;
 
-	}
+	} 
 
 	for(var i = 0 ; i < entries ; i++){
 	var contactFirstName = JSONObject.results[i].contactFirstName
 	var contactLastName = JSONObject.results[i].contactLastName
-	contacts.push(JSONObject.results[i].CID)
+	contacts.push(JSONObject.results[i].CID)	
 	var button = "<div class = 'row w-100 border border-1 border-primary h-auto p-1 g-0' onClick = showContact('" + i +"')>" +
     "<div class = 'col-4 p-0 g-0 d-flex align-items-center justify-content-center'>" +
-	"<div class = 'circle'><h3>"+ contactFirstName[0] + contactLastName[0] +"</h3></div>" + "</div>" +
+	"<div class = 'circle'><h3>"+ contactFirstName[0] + contactLastName[0] +"</h3></div>" + "</div>" + 
 	"<div class = 'col-7 d-flex flex-column justify-content-center p-0 g-0'>" +
 	"<h3 style = 'padding-left:5%'>" + contactFirstName + "</h4>" +
 	"<h3 style = 'padding-left:20%'>" + contactLastName + "</h4> </div>" +
@@ -184,7 +185,7 @@ function createInfoBoxes(contactFirstName,contactLastName,address,phoneNumber,em
 	  "<div class = 'row w-100 p-3'>" +
 	  "<div class = 'informationBox'>" +
 		"<div class = 'titleBox'>" +
-		  "<h3 id = 'contactAttribute'>Last</h3>" + "<div class = 'd-flex w-100 justify-content-end'>" +
+		  "<h3 id = 'contactAttribute'>Last</h3>" + "<div class = 'd-flex w-100 justify-content-end'>" + 
 		  "<i class='bi-pencil' onclick = 'modify(" + '"contactLastName"' + "," + CID + ")'></i>" + "</div>" +
 	"</div> " +
 		"<div class = 'contactLastName'>" +
@@ -193,7 +194,7 @@ function createInfoBoxes(contactFirstName,contactLastName,address,phoneNumber,em
 		"<div class = 'row w-100 p-3'>" +
 	"<div class = 'informationBox'>" +
 	  "<div class = 'titleBox'>" +
-		"<h3 id = 'contactAttribute'>Phone</h3>" + "<div class = 'd-flex w-100 justify-content-end'>" +
+		"<h3 id = 'contactAttribute'>Phone</h3>" + "<div class = 'd-flex w-100 justify-content-end'>" + 
 		"<i class='bi-pencil' onclick = 'modify(" + '"phoneNumber"' + "," + CID + ")'></i>" + "</div>" +
 	 "</div> " +
 	  "<div class = 'phoneNumber'>" +
@@ -249,9 +250,9 @@ function showContact(contactNumber){
 		xhr.onreadystatechange = function() {
 
 		if(this.readyState == 4 && this.status == 200) {
-
+		
 		var JSONObject = JSON.parse(xhr.responseText); //Parses the response text, converts to javascript object
-
+		
 		var contactFirstName = JSONObject.contactFirstName
 		var contactLastName  = JSONObject.contactLastName
 		var address = JSONObject.address
@@ -264,12 +265,12 @@ function showContact(contactNumber){
 		contactPhoneNumber = checkEmpty(phoneNumber)
 		contactEmail = checkEmpty(email)
 
-
+		
 		createInfoBoxes(contactFirstName,contactLastName,address,phoneNumber,email,CID)
 		}
 	}
 		xhr.send(jsonData); //Will send the data and when the state changes will recieve a response
-
+		
 	}
 	catch (err) {
 		console.log(err.message)
@@ -318,7 +319,7 @@ function addContact() {
 	}
 
 	$('.modal').modal('hide')
-
+		
 }
 
 function modify(field,CID) { //Just to replace the textvalue
@@ -330,8 +331,8 @@ function modify(field,CID) { //Just to replace the textvalue
 	"</div>"
 
 	$(fieldName).append(input)
-
-
+	
+	
 }
 
 function modifySettings(field) { //Just to replace the textvalue
@@ -343,19 +344,19 @@ function modifySettings(field) { //Just to replace the textvalue
 	"</div>"
 
 	$(fieldName).append(input)
-
+		
 }
 
-function update(fieldName,CID){ //For updating the contact
+function update(fieldName,CID){ //For updating the contact 
 
 		var field = "." + fieldName
 		var updateField = "#" + fieldName + "text"
 		var updateValue = $(updateField).val()
-
+	
 		var url = "http://159.203.70.233/LAMPAPI/UpdateContact.php"
 
 		var jsonData = JSON.stringify({"CID":CID,"field":fieldName,"value":updateValue})
-
+		
 		try {
 
 		xhr = openHTTP(url,"POST")
@@ -368,7 +369,7 @@ function update(fieldName,CID){ //For updating the contact
 			}
 		}
 		xhr.send(jsonData)
-		}
+		}	
 		catch(err) {
 			console.log(err.message) //otherwise output error
 			return;
@@ -376,7 +377,7 @@ function update(fieldName,CID){ //For updating the contact
 
 		$(field).empty()
 		$(field).append("<p id = " + field + " > " + updateValue + " </p>")
-
+		
 }
 
 
@@ -386,7 +387,7 @@ function deleteUser(){
 	var jsonData = JSON.stringify({"ID":ID})
 
 	try {
-
+		
 		xhr = openHTTP(url,"POST")
 		xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8")
 
@@ -398,7 +399,7 @@ function deleteUser(){
 			}
 		}
 		xhr.send(jsonData)
-		}
+		}	
 		catch(err) {
 			console.log(err.message) //otherwise output error
 		}
@@ -422,7 +423,7 @@ function deleteContact(CID){
 			}
 		}
 		xhr.send(jsonData)
-		}
+		}	
 
 		catch(err) {
 
@@ -440,7 +441,7 @@ function settings(fieldName) {
 	if(fieldName == "password") {
 		updateValue = md5(updateValue)
 	}
-
+	
 	var url = "http://159.203.70.233/LAMPAPI/UpdateUser.php"
 
 	var jsonData = JSON.stringify({"ID":ID,"field":fieldName,"value":updateValue})
@@ -458,7 +459,7 @@ function settings(fieldName) {
 		}
 
 		xhr.send(jsonData)
-		}
+		}	
 		catch(err) {
 			console.log(err.message) //otherwise output error
 			return;
@@ -471,14 +472,14 @@ function settings(fieldName) {
 		}
 		$(field).append("<p id = " + field + " > " + updateValue + " </p>")
 }
-
+	
 function saveCookie(){ //Need to save cookies so if user refreshes page they are still remembered
 
 	var minutes = 20; //The time to save cookie
 	var date = new Date();
-	date.setTime(date.getTime()+(minutes*60*1000));
+	date.setTime(date.getTime()+(minutes*60*1000));	
     document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",ID=" + ID + ";expires=" + date.toGMTString();
-
+    
     //Saves the cookie to keep track of the user
 }
 
@@ -487,7 +488,7 @@ function readCookie()
 	ID = -1;
 	var data = document.cookie;
 	var splits = data.split(",");
-	for(var i = 0; i < splits.length; i++)
+	for(var i = 0; i < splits.length; i++) 
 	{
 		var thisOne = splits[i].trim();
 		var tokens = thisOne.split("=");
@@ -508,7 +509,7 @@ function readCookie()
 	{
 		window.location.href = "index.html";
 	}
-
+	
 }
 
 function searchAll(){
@@ -540,9 +541,7 @@ function closeModal() {
 
 function changeSettings() {
 
-	//retrieve user data
-
-	
+	showUser()
 
 	var boxes = "<div class = 'row w-100 p-3'>" +
 	"<div class = 'informationBox'>" +
@@ -556,7 +555,7 @@ function changeSettings() {
 	  "<div class = 'row w-100 p-3'>" +
 	  "<div class = 'informationBox'>" +
 		"<div class = 'titleBox'>" +
-		  "<h3 id = 'contactAttribute'>First</h3>" + "<div class = 'd-flex w-100 justify-content-end'>" +
+		  "<h3 id = 'contactAttribute'>First</h3>" + "<div class = 'd-flex w-100 justify-content-end'>" + 
 		  "<i class='bi-pencil' onclick = 'modifySettings(" + '"firstName"' +")'></i>" + "</div>" +
 	"</div> " +
 		"<div class = 'firstName'>" +
@@ -584,9 +583,50 @@ function changeSettings() {
 		  "<div class = 'password'>" +
 		  "<button type='button' class='btn btn-primary' onclick = modifySettings(" + '"password"' +")>Change Password</button>" + "</div><br></br>"
 		  + "<button type='button' class='btn btn-danger' onclick = deleteUser()>Delete Account</button>"
-
+		
 
 	$("#settingsModal").empty();
 	$("#settingsModal").append(boxes);
 
 }
+
+function showUser() {
+
+
+	var url = "http://159.203.70.233/LAMPAPI/ShowUser.php"
+
+	var jsonData= JSON.stringify({"ID":ID})
+
+	try
+	{
+		var xhr = openHTTP(url,"POST")
+		xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+		xhr.onreadystatechange = function() {
+
+		if(this.readyState == 4 && this.status == 200) {
+		
+		var JSONObject = JSON.parse(xhr.responseText); //Parses the response text, converts to javascript object
+		
+		firstName = JSONObject.firstName
+		lastName  = JSONObject.lastName
+		address = JSONObject.address
+		userName = JSONObject.userName
+		email = JSONObject.email
+
+		firstName = checkEmpty(firstName)
+		lastName = checkEmpty(lastName)
+		address = checkEmpty(address)
+		phoneNumber = checkEmpty(phoneNumber)
+		email = checkEmpty(email)
+		}
+	}
+		xhr.send(jsonData); //Will send the data and when the state changes will recieve a response
+		
+	}
+	catch (err) {
+		console.log(err.message)
+	}
+
+}
+
