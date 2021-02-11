a50b20453156b196f1964049683edbfcf95c118d
Gregory Freitas <gregfreitas1997@gmail.com>
2021-02-10 23:48:30 -0500

    Test

--- LAMPAPI/search.php
+++ LAMPAPI/search.php
@@ -19,6 +19,7 @@
         
         $result = $conn->query($sql); #Will return an array
         
+        // This is a test.
         if ($result->num_rows > 0)
         {
             while($row = $result->fetch_assoc())
