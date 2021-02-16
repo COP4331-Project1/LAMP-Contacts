454fd4854fe4bec97b7f8e681f9bbcda42791ebe
Ryan Pattillo <ryanpattillo@Ryans-MacBook-Pro-2.local>
2021-02-15 19:39:50 -0500

    Update

--- js/code.js
+++ js/code.js
@@ -703,6 +703,7 @@ function gettingStarted(){
 
 	readCookie()
 
+	userName = "dsfsdfsd"
 	var box = "<div class = 'col-10 bg-light h-100 border border-2 border-muted rounded-3 shadow-lg bg-white rounded p-2'>" 
 	+ "<h1 style id = 'startPageTitle' style = 'text-align:center'> Welcome " + userName + "</h1>" 
 	+ "<div class = 'row w-100 p-1'>"
