776d1cda4676d9079eca4e50c9e86d09e3a4924d
Ryan Pattillo <ryanpattillo@Ryans-MacBook-Pro-2.local>
2021-02-21 14:58:18 -0500

    Update

--- js/code.js
+++ js/code.js
@@ -333,7 +333,7 @@ function closeDeleteAccount(){
 	$("#settings").modal('show')
 }
 
-function deleteAccountBox() { //Displays the dialog box for deleting a contact
+function deleteAccountModal() { //Displays the dialog box for deleting a contact
 
 	var deleteModal = "<div class='modal fade in' id='deleteAccount' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>"
 	+ "<div class='modal-dialog modal-dialog-centered' role='document'>"
@@ -674,7 +674,7 @@ function changeSettings() {
 	+"<div class = 'password'>" 
 	+"<button type='button' class='btn btn-primary' onclick = modifySettings(" + '"password"' +")>Change Password</button>" + "</div><br></br>"
 	+ "<div class = 'alert alert-primary' role = 'alert'>Date Created: " + dateCreated + "</div>" 
-	+ "<button type='button' class='btn btn-danger' onclick = deleteUser()>Delete Account</button>"
+	+ "<button type='button' class='btn btn-danger' onclick = deleteAccountModal()>Delete Account</button>"
 
 	$("#settingsModal").empty();
 	$("#settingsModal").append(boxes);
