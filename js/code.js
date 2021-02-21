f1d74baf897e9891f676ff00231d4c3227a9e348
Ryan Pattillo <ryanpattillo@Ryans-MacBook-Pro-2.local>
2021-02-21 12:18:50 -0500

    Update

--- js/code.js
+++ js/code.js
@@ -200,7 +200,7 @@ function createInfoBoxes(contactFirstName,contactLastName,address,phoneNumber,em
 	+"</div> " 
 	+"<div class = 'contactFirstName'>" 
 	+"<div class='input-group mb-1' id = 'inputs'>" +"<input type='text' class='form-control' id = 'contactFirstNameText' onchange = update('contactFirstNameText','"+CID+"') aria-describedby='inputGroup-sizing-default'>"
-	+"<h4>" + contactFirstName + "</h4>" + "</div>" + "</div>"  + "</div>" +
+	+"</div><h4>" + contactFirstName + "</h4>" + "</div>" + "</div>"  + "</div>" +
 
 	"<div class = 'row w-100 p-2'>" +
 	"<div class = 'informationBox'>" +
