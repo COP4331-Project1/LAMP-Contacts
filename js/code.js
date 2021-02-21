ea9065ca0af3b7fb46d59acfb69a3e403208fa7c
Ryan Pattillo <ryanpattillo@Ryans-MacBook-Pro-2.local>
2021-02-21 13:40:56 -0500

    Update

--- js/code.js
+++ js/code.js
@@ -208,7 +208,8 @@ function cancel(field) {
 
 function createInfoBoxes(contactFirstName,contactLastName,address,phoneNumber,email,CID,dateCreated) { //Shows the contact information when clicked on
 
-	var boxes = "<div class = 'col w-100 bg-light border border-2 border-muted id = 'showContacts'>" 
+	var boxes = "<div class = 'col w-100 bg-light border border-2 border-muted' id = 'showContacts'>" 
+
 	+"<div class = 'row w-100 p-2'>" 
 	+"<div class = 'informationBox'>" 
 	+"<div class = 'titleBox'>" 
