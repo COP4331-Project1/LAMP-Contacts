bbf48f736bb68a457543af90158e351ef63d0cf9
Ryan Pattillo <ryanpattillo@Ryans-MacBook-Pro-2.local>
2021-02-21 11:08:31 -0500

    Update

--- js/code.js
+++ js/code.js
@@ -263,7 +263,7 @@ function deleteAlertBox(CID) { //Displays the dialog box for deleting a contact
 
 
 function checkEmpty(string) { //Function to see 
-
+	console.log(string.length)
 	if((!string || 0 === string.length)) return "N/A"
 	else return string
 
